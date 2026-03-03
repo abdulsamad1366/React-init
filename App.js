41b00aa64fa0461bfa346e966a9ac35362a8b686
MohdAnjum <Welcome@94124>
2026-03-03 15:21:02 +0530

    start

---
+++ App.js
@@ -0,0 +1,15 @@
+const parent = React.createElement(
+    "div" , {id : "parent"} , [React.createElement(
+        "div" ,{ id : "child" } , [ React.createElement(
+            "h1" , {} , "hello h1 tag "), React.createElement(
+            "h2" , {} , "hello h2 tag ")]),React.createElement(
+        "div" ,{ id : "child2" } , [ React.createElement(
+            "h1" , {} , "hello h1 tag "), React.createElement(
+            "h2" , {} , "hello h2 tag ")])] ) ;
+
+
+
+// const heading = React.createElement("h1", { id: "heading" }, " hello World From React!");
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
