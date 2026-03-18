86f0b377cb3292e1220a01d7c82c07cb1395587f
abdulsamad1366 <dev.abdulsamad.cs@gmail.com>
2026-03-18 21:48:20 +0530

    3

--- src/components/ResCard.js
+++ src/components/ResCard.js
@@ -9,7 +9,7 @@ const ResCard = (dets) => {
             <img className="res-logo" src={CDN_URL + cloudinaryImageId} />
             <h3>{name}</h3>
             <h4>{cuisines.join(", ")}</h4>
-            <h4>{avgRating} stars</h4>
+            <h4>{avgRating} Stars</h4>
             <h4>{costForTwo}</h4>
             <h4>{resData?.card.card.info.sla.slaString}</h4>
         </div>
